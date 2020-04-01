478b1f744db88f44e55320acac9f6053ad198617
Your Name <Student_MA117@Student-MA117.local>
2020-04-01 12:36:02 -0400

    added access api urls for requests

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
 export default class extends Component{
+  constructor(props){
+    super(props);
+
+    this.state ={
+      data:[]
+    }
+
+   // access urls
+   //1. Margery Tyrell's PoB
+   let one="http://anapioficeandfire.com/api/characters/16";
+   //2. House Targaryen location
+   let two="http://www.anapioficeandfire.com/api/houses/378";
+   //3. House Lannister's Coat of Arms
+   let three="http://www.anapioficeandfire.com/api/houses/229";
+   //4. House Baratheon's 2nd cmd
+   let four="http://www.anapioficeandfire.com/api/houses/17";
+   //5. Robert Baratheon's 2nd alias
+   let five="http://www.anapioficeandfire.com/api/characters/901";
+   //6. House Stark's Founder
+   let six="http://www.anapioficeandfire.com/api/houses/362";
+   // 7. Catelyn Stark's PoV book titles
+   let seve="http://www.anapioficeandfire.com/api/characters/232";
+   
+
+  }
   render() {
     return(
       <div>
