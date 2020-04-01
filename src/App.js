569a1790d54fb51c2a5dc895408db88a89ca8035
Your Name <Student_MA117@Student-MA117.local>
2020-04-01 12:06:02 -0400

    installed axios

--- src/App.js
+++ src/App.js
@@ -2,5 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 
 export default class extends Component{
-
+  render() {
+    return(
+      <div>
+      <h1>Game of Fetches</h1>
+      <h4><em>**GoT threw away 8 years of my life with the ring of a bell**</em> </h4>
+      </div>
+    )
+  }
+    
 }
